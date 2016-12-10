8df8aca877ada2853355e13bbc742872d21c6b6a
liuxinle <liucqust@163.com>
2016-12-10 23:36:51 +0800

    add dbHelper and the bean of table

---
+++ src/com/coolweather/app/model/County.java
@@ -0,0 +1,37 @@
+package com.coolweather.app.model;
+
+public class County {
+	private int id;
+	private String countyName;
+	private String countyCode;
+	private int cityCode;
+	public County() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getCountyName() {
+		return countyName;
+	}
+	public void setCountyName(String countyName) {
+		this.countyName = countyName;
+	}
+	public String getCountyCode() {
+		return countyCode;
+	}
+	public void setCountyCode(String countyCode) {
+		this.countyCode = countyCode;
+	}
+	public int getCityCode() {
+		return cityCode;
+	}
+	public void setCityCode(int cityCode) {
+		this.cityCode = cityCode;
+	}
+	
+}
